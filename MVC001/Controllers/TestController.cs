bba86484a53213460faa841e76c30598f5fbe98b
mariam-diab <mariam.diab@ejust.edu.eg>
2024-08-30 00:58:21 +0300

    first mvc session

---
+++ MVC001/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC001.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ContentResult MyContent()
+        {
+            return Content("<h1>WELCOME TO MVC!<h1/>", "text/html");
+        }
+
+        public JsonResult MyJson() 
+        {
+            string[] names = { "Ahmed", "Ali", "Sayed" };
+            return Json(names);
+        }
+        //public IActionResult()
+        //{
+        //    return NotFound();
+        //}
+    }
+}
